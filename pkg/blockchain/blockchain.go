195e0b180574fe7fb9e462e83323749815532c63
Eddie Ho <noodleslove@icloud.com>
2022-09-01 15:24:03 -0700

    Improve NewBlockchain for persistence

--- pkg/blockchain/blockchain.go
+++ pkg/blockchain/blockchain.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"github.com/boltdb/bolt"
+	"github.com/noodleslove/blockchain-go/pkg/utils"
+)
+
+const dbFile = "blockchain.db"
+const blockBucket = "blocks"
+
 type Blockchain struct {
-	blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
 // AddBlock saves the provided data as a block in the blockchain
@@ -13,7 +22,30 @@ func (bc *Blockchain) AddBlock(data string) {
 
 // NewBlockchain returns a blockchain with a genesis block
 func NewBlockchain() *Blockchain {
-	return &Blockchain{
-		blocks: []*Block{NewGenesisBlock()},
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+	utils.Check(err)
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blockBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blockBucket))
+			utils.Check(err)
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			err = b.Put("l", genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+
+	bc := Blockchain{
+		tip: tip,
+		db:  db,
 	}
+	return &bc
 }
