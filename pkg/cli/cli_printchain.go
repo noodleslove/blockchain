ddd379b1d556fb783cc799cc48fd9d8fb75504e2
Eddie Ho <noodleslove@icloud.com>
2022-09-01 16:45:50 -0700

    Implement printchain

---
+++ pkg/cli/cli_printchain.go
@@ -0,0 +1,27 @@
+package cli
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/noodleslove/blockchain-go/pkg/blockchain"
+)
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	for {
+		block := bci.Next()
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := blockchain.NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
+}
