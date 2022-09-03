5c9ac1b8759afb723eb4394b9b4f07d2de6631ba
Eddie Ho <noodleslove@icloud.com>
2022-09-03 16:49:04 -0700

    Implement basic Transaction

---
+++ pkg/transaction/transaction.go
@@ -0,0 +1,55 @@
+package transaction
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+
+	"github.com/noodleslove/blockchain-go/pkg/utils"
+)
+
+const subsidy = 10
+
+type Transaction struct {
+	ID   []byte
+	Vin  []TXInput
+	Vout []TXOutput
+}
+
+// NewCoinbaseTX creates a new coinbase transaction
+func NewCoinbaseTx(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Reward to '%s'", to)
+	}
+
+	txin := TXInput{
+		Txid:      []byte{},
+		Vout:      -1,
+		ScriptSig: data,
+	}
+	txout := TXOutput{
+		Value:        subsidy,
+		ScriptPubKey: to,
+	}
+	tx := Transaction{
+		ID:   nil,
+		Vin:  []TXInput{txin},
+		Vout: []TXOutput{txout},
+	}
+	tx.SetID()
+
+	return &tx
+}
+
+// SetID sets ID of a transaction
+func (tx *Transaction) SetID() {
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	enc := gob.NewEncoder(&encoded)
+	err := enc.Encode(tx)
+	utils.Check(err)
+	hash = sha256.Sum256(encoded.Bytes())
+	tx.ID = hash[:]
+}
