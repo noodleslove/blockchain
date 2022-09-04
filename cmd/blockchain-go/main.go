7a727a871666748c58e402f6ea64853424f0f4b9
Eddie Ho <noodleslove@icloud.com>
2022-09-03 23:07:38 -0700

    Fix it

--- cmd/blockchain-go/main.go
+++ cmd/blockchain-go/main.go
@@ -1,14 +1,8 @@
 package main
 
-import (
-	"github.com/noodleslove/blockchain-go/pkg/blockchain"
-	"github.com/noodleslove/blockchain-go/pkg/cli"
-)
+import "github.com/noodleslove/blockchain-go/pkg/cli"
 
 func main() {
-	bc := blockchain.NewBlockchain()
-	defer bc.CloseDB()
-
-	cli := cli.NewCLI(bc)
+	cli := cli.CLI{}
 	cli.Run()
 }
