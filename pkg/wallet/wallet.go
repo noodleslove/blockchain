105496481cf52c1d0f83b7676d013fd8ca764886
Eddie Ho <noodleslove@icloud.com>
2022-09-06 22:36:59 -0700

    Implement Wallet.GetAddress

--- pkg/wallet/wallet.go
+++ pkg/wallet/wallet.go
@@ -4,15 +4,22 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/noodleslove/blockchain-go/pkg/utils"
+	"golang.org/x/crypto/ripemd160"
 )
+
+const version = byte(0x00)
+const addressChecksumLen = 4
 
 type Wallet struct {
 	PrivateKey ecdsa.PrivateKey
 	PublicKey  []byte
 }
 
+// Returns a new wallet
 func NewWallet() *Wallet {
 	private, public := newKeyPair()
 	return &Wallet{
@@ -21,6 +28,7 @@ func NewWallet() *Wallet {
 	}
 }
 
+// Helper function generates new private, public key pair
 func newKeyPair() (ecdsa.PrivateKey, []byte) {
 	curve := elliptic.P256()
 	private, err := ecdsa.GenerateKey(curve, rand.Reader)
@@ -28,4 +36,37 @@ func newKeyPair() (ecdsa.PrivateKey, []byte) {
 	pubKey := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
 
 	return *private, pubKey
+}
+
+// GetAddress returns wallet address
+func (w *Wallet) GetAddress() []byte {
+	pubKeyHash := HashPubKey(w.PublicKey)
+
+	versionedPayload := append([]byte{version}, pubKeyHash...)
+	checksum := checksum(versionedPayload)
+
+	fullPayload := append(versionedPayload, checksum...)
+	address := []byte(base58.Encode(fullPayload))
+
+	return address
+}
+
+// Helper function hashes public key
+func HashPubKey(pubKey []byte) []byte {
+	publicSHA256 := sha256.Sum256(pubKey)
+
+	RIPEMD160Hasher := ripemd160.New()
+	_, err := RIPEMD160Hasher.Write(publicSHA256[:])
+	utils.Check(err)
+	publicRIPEMD160 := RIPEMD160Hasher.Sum(nil)
+
+	return publicRIPEMD160
+}
+
+// Helper function calculates checksum
+func checksum(payload []byte) []byte {
+	firstSHA := sha256.Sum256(payload)
+	secondSHA := sha256.Sum256(firstSHA[:])
+
+	return secondSHA[:addressChecksumLen]
 }
