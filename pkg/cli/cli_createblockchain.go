571fdb9f549d6b65adbb6fd92d6d939c181d0414
Eddie Ho <noodleslove@icloud.com>
2022-09-10 16:36:21 -0700

    Improve cli

--- pkg/cli/cli_createblockchain.go
+++ pkg/cli/cli_createblockchain.go
@@ -2,12 +2,22 @@ package cli
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/noodleslove/blockchain-go/pkg/blockchain"
+	"github.com/noodleslove/blockchain-go/pkg/wallet"
 )
 
 func (cli *CLI) createBlockchain(address string) {
+	if !wallet.ValidateAddress(address) {
+		log.Panic("ERROR: Address is not valid")
+	}
+
 	bc := blockchain.CreateBlockchain(address)
-	bc.CloseDB()
+	defer bc.CloseDB()
+
+	utxoSet := blockchain.UTXOSet{Blockchain: bc}
+	utxoSet.Reindex()
+
 	fmt.Println("Done!")
 }
--- pkg/cli/cli_send.go
+++ pkg/cli/cli_send.go
@@ -2,12 +2,21 @@ package cli
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/noodleslove/blockchain-go/pkg/blockchain"
 	"github.com/noodleslove/blockchain-go/pkg/transaction"
+	"github.com/noodleslove/blockchain-go/pkg/wallet"
 )
 
 func (cli *CLI) send(from, to string, amount int) {
+	if !wallet.ValidateAddress(from) {
+		log.Panic("ERROR: Sender address is not valid")
+	}
+	if !wallet.ValidateAddress(to) {
+		log.Panic("ERROR: Recipient address is not valid")
+	}
+
 	bc := blockchain.NewBlockchain()
 	defer bc.CloseDB()
 
