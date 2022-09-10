ae624c6e38dad643f376e86c43790316d1e4938c
Eddie Ho <noodleslove@icloud.com>
2022-09-10 12:26:23 -0700

    Update blockchain interface

--- pkg/transaction/blockchain_interface.go
+++ pkg/transaction/blockchain_interface.go
@@ -3,6 +3,5 @@ package transaction
 import "crypto/ecdsa"
 
 type blockchain interface {
-	FindSpendableOutputs(pubKeyHash []byte, amount int) (int, map[string][]int)
 	SignTransaction(tx *Transaction, privKey ecdsa.PrivateKey)
 }
