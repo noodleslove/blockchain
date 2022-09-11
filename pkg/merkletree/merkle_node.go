44fbffc0d1b9a94124bcb7f35e86633826dbfac8
Eddie Ho <noodleslove@icloud.com>
2022-09-10 17:49:05 -0700

    Implement merkle tree

---
+++ pkg/merkletree/merkle_node.go
@@ -0,0 +1,27 @@
+package merkletree
+
+import "crypto/sha256"
+
+type MerkleNode struct {
+	Left  *MerkleNode
+	Right *MerkleNode
+	Data  []byte
+}
+
+func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
+	mNode := MerkleNode{}
+
+	if left == nil && right == nil {
+		hash := sha256.Sum256(data)
+		mNode.Data = hash[:]
+	} else {
+		prevHashes := append(left.Data, right.Data...)
+		hash := sha256.Sum256(prevHashes)
+		mNode.Data = hash[:]
+	}
+
+	mNode.Left = left
+	mNode.Right = right
+
+	return &mNode
+}
